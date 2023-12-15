aeeae5a6865108d9f0464c26c40aedbd46a5f1be
Nebur <rubenmasalmira@gmail.com>
2023-12-15 16:35:30 +0100

    Cambios para mostrar fotos railway

--- Odacrem/settings.py
+++ Odacrem/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Indicamos a Django que las imagenes se guardan en esta carpeta
 MEDIA_URL='/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
